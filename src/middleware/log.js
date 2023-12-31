e542c21f08b2aa392ca3a31440e563ada57a5cad
MrZwen <gedeagusadityadharma@gmail.com>
2023-12-31 19:08:43 +0800

    Update Log +timestamp & +respon time

--- src/middleware/log.js
+++ src/middleware/log.js
@@ -1,7 +1,15 @@
 const logRequest = (req, res, next) => {
-    console.log('Request ke Path', req.path)
-    console.log('Request Type:', req.method)
+    const start = Date.now();
+    const timestamp = new Date().toLocaleString();
+    console.log(`[${timestamp}] Request to Path: ${req.path}`)
+    console.log(`[${timestamp}] Request Type: ${req.method}`)
     next()
+
+    res.on('finish', () => {
+        const end = Date.now();
+        const responseTime = end - start;
+        console.log(`[${timestamp}] Response Time: ${responseTime}ms`);
+    });
 }
 
 module.exports = logRequest
