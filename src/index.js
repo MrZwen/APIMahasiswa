7d764b8a984c2cd054b53e9e0859d32386cd1ce7
MrZwen <gedeagusadityadharma@gmail.com>
2024-01-01 16:14:00 +0800

    Add Host

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 require('dotenv').config()
 const PORT = process.env.PORT || 5000
+const HOST = process.env.NODE_ENV !== 'production' ? 'localhost' : '0.0.0.0'
 const express = require('express')
 const mahasiswaRoutes = require('./routes/MahasiswaRoutes')
 const prodiRoutes = require('./routes/ProdiRoutes')
@@ -15,6 +16,7 @@ app.use('/mahasiswa', mahasiswaRoutes)
 app.use('/prodi', prodiRoutes)
 app.use('/jurusan', jurusanRoutes)
 
-app.listen(PORT, () => {
-    console.log(`Server berhasil di jalankan,pada port ${PORT}!`);
+app.listen(PORT, HOST, () => {
+    console.log(`Server berjalan pada host ${HOST}`)
+    console.log(`Server berhasil di jalankan,pada port ${PORT}!`)
 });
