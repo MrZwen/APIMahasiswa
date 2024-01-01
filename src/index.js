301eb6b220066e1a0e7f61eadd8941e465b0372b
MrZwen <gedeagusadityadharma@gmail.com>
2024-01-01 16:39:51 +0800

    ADD host

--- src/index.js
+++ src/index.js
@@ -1,22 +1,25 @@
-require('dotenv').config()
-const PORT = process.env.PORT || 5000
-const HOST = process.env.NODE_ENV !== 'production' ? 'localhost' : '0.0.0.0'
-const express = require('express')
-const mahasiswaRoutes = require('./routes/MahasiswaRoutes')
-const prodiRoutes = require('./routes/ProdiRoutes')
-const jurusanRoutes = require('./routes/JurusanRoutes')
-const MiddlewareLog = require('./middleware/log')
+require('dotenv').config();
+const http = require('http');
+const PORT = process.env.PORT || 5000;
+const HOST = '0.0.0.0';
+const express = require('express');
+const mahasiswaRoutes = require('./routes/MahasiswaRoutes');
+const prodiRoutes = require('./routes/ProdiRoutes');
+const jurusanRoutes = require('./routes/JurusanRoutes');
+const MiddlewareLog = require('./middleware/log');
 
-const app = express()
+const app = express();
 
-app.use(MiddlewareLog)
-app.use(express.json())
+app.use(MiddlewareLog);
+app.use(express.json());
 
-app.use('/mahasiswa', mahasiswaRoutes)
-app.use('/prodi', prodiRoutes)
-app.use('/jurusan', jurusanRoutes)
+app.use('/mahasiswa', mahasiswaRoutes);
+app.use('/prodi', prodiRoutes);
+app.use('/jurusan', jurusanRoutes);
 
-app.listen(PORT, HOST, () => {
-    console.log(`Server berjalan pada host ${HOST}`)
-    console.log(`Server berhasil di jalankan,pada port ${PORT}!`)
+const server = http.createServer(app);
+
+server.listen(PORT, HOST, () => {
+    console.log(`Server berjalan pada host ${HOST}`);
+    console.log(`Server berhasil dijalankan, pada port ${PORT}!`);
 });
